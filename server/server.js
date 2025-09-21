bdc8de6e14940434b33cbc21c67b881eae524615
shibsrc <shibjyoti252@gmail.com>
2025-09-21 21:03:02 +0530

    Server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://ayur-sutra-seven.vercel.app", credentials: true }));
 
 
 app.use("/api/auth", require("./routes/authRoutes"));
