f780aa6da3cfc693af3e7809fb22514956880620
shibsrc <shibjyoti252@gmail.com>
2025-09-21 21:30:06 +0530

    D1

--- server/server.js
+++ server/server.js
@@ -4,11 +4,27 @@ const cors = require("cors");
 require('dotenv').config();
 
 connectDB();
+const allowedOrigins = [
+  "https://ayur-sutra-seven.vercel.app",
+  "http://localhost:5173"
+];
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "https://ayur-sutra-seven.vercel.app", credentials: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/patient", require("./routes/patientRoutes"));
